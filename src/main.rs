bc25b6c22ddafeb08c94821c19782facd9c9ce3d
Kyle Larsen <larsenkg@gmail.com>
2024-12-27 20:06:17 +0000

    Modified boids_behavior to use spatial hash map. Can run 10,000 boids at 20 to 30 fps (without colors).

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use bevy::asset::AssetMetaCheck;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
+use bevy::utils::HashMap;
 use bevy::window::PrimaryWindow;
 
-const BOID_NUMBER: usize = 3000;
+use bevy_boids::voxel::VoxelHashMap;
+
+const BOID_NUMBER: usize = 10000;
 const BOID_RADIUS: f32 = 10.0;
 const BOID_SECTION_DEG: f32 = 10.0;
 const BOID_MAX_SPEED: f32 = 600.0;
 const BOID_MIN_SPEED: f32 = 50.0;
-// const BOID_SPEED_C
 const BOID_SEPARATION_FACTOR: f32 = 0.05;
 const BOID_ALIGNMENT_FACTOR: f32 = 0.05;
 const BOID_COHESION_FACTOR: f32 = 0.005;
@@ -41,23 +43,34 @@ fn main() {
             ..default()
         }))
         .add_plugins((FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin::default()))
-        .add_systems(Startup, (setup, spawn_boids))
+        .add_systems(Startup, (setup, spawn_boids).chain())
         // .add_systems(Update, (avoid_boundary, move_boids).chain())
         .add_systems(
             Update,
-            (boids_behavior, avoid_boundary, move_boids, color_boids).chain(),
+            (boids_behavior, avoid_boundary, move_boids).chain(), // color_boids
         )
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
+    commands.insert_resource(VoxelHashMap {
+        map: Default::default(),
+        cell_size: 2. * BOID_ALIGNMENT_RADIUS / 3., // 3 cells should equal alignment diameter
+                                                    // cell_size: BOID_SEPARATION_RADIUS / 2.,
+    });
+
+    // Render voxel cell boundaries
+    if DEBUG {
+        // commands.spawn();
+    }
 }
 
 fn spawn_boids(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    mut voxels: ResMut<VoxelHashMap>,
 ) {
     let shape = meshes.add(CircularSector::new(
         BOID_RADIUS,
@@ -81,7 +94,7 @@ fn spawn_boids(
         // let v = translation.cross(Vec3::Z).normalize() * BOID_MAX_SPEED / 6.;
         let v = Vec3::ZERO;
 
-        commands
+        let entity = commands
             .spawn((
                 Boid {
                     avoid_radius: BOID_SEPARATION_RADIUS,
@@ -113,17 +126,78 @@ fn spawn_boids(
                     ),
                     || DEBUG,
                 );
-            });
+            })
+            .id();
+
+        // Add to voxel hash map
+        voxels.insert(translation.xy(), entity);
+    }
+    println!("Number of voxels: {}", voxels.map.len());
+}
+
+fn boids_behavior_fast(
+    mut q_boids: Query<(&mut Boid, &Transform, &Velocity)>,
+    voxels: Res<VoxelHashMap>,
+) {
+    let mut velocity: HashMap<(i64, i64), Vec2> = HashMap::new();
+    let mut position: HashMap<(i64, i64), Vec2> = HashMap::new();
+    // let mut n_neighbors: HashMap<(i64, i64), usize> = HashMap::new();
+
+    for (pos, entities) in voxels.map.iter() {
+        let (x, y) = pos;
+        let mut n = 0;
+        for entity in entities {
+            if let Ok((_boid, transform, vel)) = q_boids.get(*entity) {
+                n += 1;
+                *velocity.entry((*x, *y)).or_insert(vel.0.xy()) += vel.0.xy();
+                *position
+                    .entry((*x, *y))
+                    .or_insert(transform.translation.xy()) += transform.translation.xy();
+            }
+        }
+        *velocity.entry((*x, *y)).or_insert(Vec2::ZERO) /= n as f32;
+        *position.entry((*x, *y)).or_insert(Vec2::ZERO) /= n as f32;
+        // n_neighbors.insert((*x, *y), n);
+    }
+
+    for (mut boid, transform, _) in q_boids.iter_mut() {
+        let mut separation_acc = Vec2::ZERO;
+        let mut alignment_acc = Vec2::ZERO;
+        let mut position_acc = Vec2::ZERO;
+
+        let mut n_neighbors = 0;
+        let neighbors =
+            voxels.get_neighbor_keys_within(transform.translation.xy(), BOID_ALIGNMENT_RADIUS);
+
+        for key in neighbors {
+            let Some(other_position) = position.get(&key) else {
+                continue;
+            };
+            let Some(other_velocity) = velocity.get(&key) else {
+                continue;
+            };
+
+            let distance = transform.translation.xy().distance_squared(*other_position);
+            if distance < boid.avoid_radius * boid.avoid_radius {
+                separation_acc += transform.translation.xy() - other_position;
+            } else if distance < boid.align_radius * boid.align_radius {
+                alignment_acc += other_velocity;
+                position_acc += other_position;
+                n_neighbors += 1;
+            }
+        }
+
+        boid.separation_accumulator = separation_acc.extend(0.);
+        boid.alignment_accumulator = alignment_acc.extend(0.);
+        boid.position_accumulator = position_acc.extend(0.);
+        boid.n_neighbors = n_neighbors;
     }
 }
 
 fn boids_behavior(
     mut q_boids: Query<(&mut Boid, &Transform)>,
     q_boids_other: Query<(&Transform, &Velocity), With<Boid>>,
-    // mut paramset: ParamSet<(
-    //     Query<(&Boid, &Transform, &mut Velocity)>,
-    //     Query<(&Boid, &Transform, &Velocity)>,
-    // )>,
+    voxels: Res<VoxelHashMap>,
 ) {
     for (mut boid, transform) in q_boids.iter_mut() {
         let mut separation = Vec3::ZERO;
@@ -132,20 +206,23 @@ fn boids_behavior(
 
         let mut n_neighbors = 0;
 
-        for (other_transform, other_velocity) in q_boids_other.iter() {
+        let neighbors = voxels.get_neighbor_entities(transform.translation.xy());
+
+        // for (other_transform, other_velocity) in q_boids_other.iter() {
+        for other_entity in neighbors {
+            let (other_transform, other_velocity) = q_boids_other.get(other_entity).unwrap();
             // if boid == other_boid {
             //     continue;
             // }
             // Separation
-            let distance = transform.translation.distance(other_transform.translation);
-            if distance <= f32::EPSILON {
-                // Don't count self
-                continue;
-            } else if distance < boid.avoid_radius {
+            let distance = transform
+                .translation
+                .distance_squared(other_transform.translation);
+            if distance < boid.avoid_radius * boid.avoid_radius {
                 separation += transform.translation - other_transform.translation;
             }
             // Alignment
-            else if distance < boid.align_radius {
+            else if distance < boid.align_radius * boid.align_radius {
                 alignment += other_velocity.0;
                 position += other_transform.translation;
                 n_neighbors += 1;
@@ -174,8 +251,12 @@ fn boids_behavior(
     }
 }
 
-fn move_boids(time: Res<Time>, mut query: Query<(&mut Boid, &mut Transform, &mut Velocity)>) {
-    for (mut boid, mut transform, mut velocity) in query.iter_mut() {
+fn move_boids(
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut Boid, &mut Transform, &mut Velocity)>,
+    mut voxels: ResMut<VoxelHashMap>,
+) {
+    for (entity, mut boid, mut transform, mut velocity) in query.iter_mut() {
         // Separation
         velocity.0 += boid.separation_accumulator * BOID_SEPARATION_FACTOR;
 
@@ -208,7 +289,9 @@ fn move_boids(time: Res<Time>, mut query: Query<(&mut Boid, &mut Transform, &mut
             velocity.0 = velocity.0.normalize() * BOID_MIN_SPEED;
         }
 
+        let old_translation = transform.translation.xy();
         transform.translation += velocity.0 * time.delta_secs();
+        voxels.update_entity(old_translation, transform.translation.xy(), entity);
 
         // Rotate to face the direction of the velocity vector
         let angle = ops::atan2(velocity.0.y, velocity.0.x);
